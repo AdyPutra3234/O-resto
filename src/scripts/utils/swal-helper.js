221a3e71d87938641b4cbb922e7e350e2fae7516
AdyPutra3234 <romadona.xtl4@gmail.com>
2021-06-29 22:03:16 +0700

    edit swal-helper.js

--- src/scripts/utils/swal-helper.js
+++ src/scripts/utils/swal-helper.js
@@ -1,4 +1,5 @@
-import Swal from 'sweetalert2';
+import Swal from 'sweetalert2/dist/sweetalert2.min';
+import 'sweetalert2/dist/sweetalert2.min.css';
 
 const sweetAlert = {
   successMessage(text, callbck) {
